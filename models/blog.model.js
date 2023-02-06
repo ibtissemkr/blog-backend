72685a684a9f0f5afe004aff197f298edcf6eabe
ibtissemkr <ibtissem.kraiem@esprit.tn>
2023-02-06 23:31:36 +0100

    feature : create blog model

---
+++ models/blog.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    picture: {
+        type: String,
+        required: true,
+    }
+
+});
+
+const Blog = mongoose.model('Blog', BlogSchema);
+module.exports = Blog;
